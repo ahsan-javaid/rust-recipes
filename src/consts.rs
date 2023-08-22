e026d7a99cb5447d12614892185d896a9a263eaa
Ahsan Javaid <ahsan786cs@gmail.com>
2023-08-22 22:47:56 +0500

    Create consts.rs

---
+++ src/consts.rs
@@ -0,0 +1,7 @@
+const NUM_OF_MONTHS: u8 = 12;
+static SEASONS: [&str;4] = ["Spring", "Summer", "Fall", "Winter"];
+
+fn main() {
+    println!("{}", NUM_OF_MONTHS);
+    println!("{:?}", SEASONS);
+}
