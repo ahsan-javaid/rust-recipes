2fa45b96d171d3f68d3365d468ce09de09ea83cb
Ahsan Javaid <ahsan786cs@gmail.com>
2024-02-10 09:10:30 +0500

    Create polymorphism.rs

---
+++ src/polymorphism.rs
@@ -0,0 +1,67 @@
+
+// version: 1.76.0
+// Trait example
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
+
+pub trait Draw { 
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for c in self.components.iter() {
+            c.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("draw in button box");
+        // code to actually draw a button
+    }
+}
+
+struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("draw in select box");
+        // code to actually draw a select box
+    }
+}
